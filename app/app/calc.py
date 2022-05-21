3d033f4c139be0b642c62fdc35f74398b887829e
lukorito <jonesdeelder@gmail.com>
2022-05-21 19:45:07 +0300

    Added throwaway test cases

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+
+"""Document created to test the code"""
+"""#!Throwaway code"""
+
+
+def add_numbers(x, y):
+    """Add two numbers"""
+    return x + y
+
+
+def subtract_numbers(x, y):
+    """Subtract two numbers"""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+
+"""#!Throwaway file for testing"""
+
+from django.test import TestCase
+
+from app.calc import add_numbers, subtract_numbers
+
+
+class CalcTests(TestCase):
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        self.assertEqual(add_numbers(3, 8), 11)
+        self.assertEqual(add_numbers(4, 4), 8)
+
+    def test_subtract_numbers(self):
+        """Test that two numbers are subtracted from each other and returned"""
+        self.assertEqual(subtract_numbers(3, 8), -5)
+        self.assertEqual(subtract_numbers(4, 4), 0)
